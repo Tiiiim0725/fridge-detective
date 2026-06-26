bf9c8f7fafb6dcf138b1b65ea4d5b79b140e9401
Tiiiim111 <1627482139@qq.com>
2026-06-26 22:39:02 +0800

    soften roadshow tab accent

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,7 +8,7 @@ import { IconSymbol } from '@/components/ui/icon-symbol'
 
 type TabSymbolName = Parameters<typeof IconSymbol>[0]['name']
 
-const NAV_ACTIVE_COLOR = '#d27735'
+const NAV_ACTIVE_COLOR = '#dc8442'
 
 function NavIcon({
   focused,
